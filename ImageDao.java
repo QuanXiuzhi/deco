3f81a82d12ec6978c892c971621b2feb3bdae9cb
jomoongi <39145544+jomoongi@users.noreply.github.com>
2018-05-30 16:09:48 +0900

    Add files via upload

---
+++ ImageDao.java
@@ -0,0 +1,197 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.websocket.Session;
+
+import Vo.ImageVo;
+import connection.OracleXEConnection;
+
+public class ImageDao {
+	
+	StringBuffer sb=new StringBuffer();
+	ResultSet rs=null;
+	Connection conn=null;
+	PreparedStatement pstmt=null;
+	
+	/*select p.pt_title, i.img_temp1, p.pt_like, p.pt_hit
+	from post p, image i
+	where p.pt_no=i.pt_no and pt_place=?
+	order by pt_create_date asc*/
+	
+	/*select *
+	  2  from(select p.pt_title, i.img_temp1, p.pt_like, p.pt_hit
+	  3  from post p, image i
+	  4  where p.pt_no=i.pt_no
+	  5  order by pt_hit desc)
+	  6  where rownum<=3;*/
+		/*select *
+	  from(select p.pt_title, i.img_temp1, p.pt_like, p.pt_hit
+	  from post p, image i
+	  where p.pt_no=i.pt_no
+	  order by pt_hit desc)
+	  where rownum<=3;*/
+	public ImageDao() {
+		conn=OracleXEConnection.getInstance().getConnection();
+	}
+	
+	public ArrayList<ImageVo> getImgTitle(String pt_place) {
+		ArrayList<ImageVo> list=new ArrayList<>();
+		sb.setLength(0);
+		sb.append("select p.pt_title, i.img_temp1, p.pt_like, p.pt_hit ");
+		sb.append("from post p, image i ");
+		sb.append("where p.pt_no=i.pt_no and pt_place=? ");
+		sb.append("order by pt_create_date asc ");
+		
+		ImageVo vo=null;
+		try {
+			pstmt=conn.prepareStatement(sb.toString());
+			pstmt.setString(1, pt_place);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+			
+			int pt_hit=rs.getInt("pt_hit");
+			int pt_like=rs.getInt("pt_like");
+			String img_temp1=rs.getString("img_temp1");
+			String pt_title=rs.getString("pt_title");
+			
+			vo=new ImageVo(pt_place, img_temp1, pt_title, pt_like, pt_hit);
+			list.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+			//조회순정렬
+	public ArrayList<ImageVo> getHits(String pt_place) {
+		ArrayList<ImageVo> list=new ArrayList<>();
+		sb.setLength(0);
+		sb.append("select p.pt_title, i.img_temp1, p.pt_like, p.pt_hit ");
+		sb.append("from post p, image i ");
+		sb.append("where p.pt_no=i.pt_no and pt_place=? ");
+		sb.append("order by pt_hit desc ");
+		
+		ImageVo vo=null;
+		try {
+			pstmt=conn.prepareStatement(sb.toString());
+			pstmt.setString(1, pt_place);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+			
+			int pt_hit=rs.getInt("pt_hit");
+			int pt_like=rs.getInt("pt_like");
+			String img_temp1=rs.getString("img_temp1");
+			String pt_title=rs.getString("pt_title");
+			
+			vo=new ImageVo(pt_place, img_temp1, pt_title, pt_like, pt_hit);
+			list.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+		//좋아요 정렬
+	public ArrayList<ImageVo> getLike(String pt_place) {
+		ArrayList<ImageVo> list=new ArrayList<>();
+		sb.setLength(0);
+		sb.append("select p.pt_title, i.img_temp1, p.pt_like, p.pt_hit ");
+		sb.append("from post p, image i ");
+		sb.append("where p.pt_no=i.pt_no and pt_place=? ");
+		sb.append("order by pt_like desc ");
+		
+		ImageVo vo=null;
+		try {
+			pstmt=conn.prepareStatement(sb.toString());
+			pstmt.setString(1, pt_place);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+			
+			int pt_hit=rs.getInt("pt_hit");
+			int pt_like=rs.getInt("pt_like");
+			String img_temp1=rs.getString("img_temp1");
+			String pt_title=rs.getString("pt_title");
+			
+			vo=new ImageVo(pt_place, img_temp1, pt_title, pt_like, pt_hit);
+			list.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+			//좋아요 상위 3 조회
+	public ArrayList<ImageVo> getLikeThree() {
+		ArrayList<ImageVo> list=new ArrayList<>();
+		sb.setLength(0);
+		sb.append("select * ");
+		sb.append("from(select p.pt_place, p.pt_title, i.img_temp1, p.pt_like, p.pt_hit ");
+		sb.append("from post p, image i ");
+		sb.append("where p.pt_no=i.pt_no ");
+		sb.append("order by pt_like desc) ");
+		sb.append("where rownum<=3 ");
+		
+		ImageVo vo=null;
+		try {
+			pstmt=conn.prepareStatement(sb.toString());
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+			
+			String pt_place=rs.getString("pt_place");
+			int pt_hit=rs.getInt("pt_hit");
+			int pt_like=rs.getInt("pt_like");
+			String img_temp1=rs.getString("img_temp1");
+			String pt_title=rs.getString("pt_title");
+			
+			vo=new ImageVo(pt_place, img_temp1, pt_title, pt_like, pt_hit);
+			list.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+			//조회순 상위 3 조회
+	public ArrayList<ImageVo> getHitThree() {
+		ArrayList<ImageVo> list=new ArrayList<>();
+		sb.setLength(0);
+		sb.append("select * ");
+		sb.append("from(select p.pt_place, p.pt_title, i.img_temp1, p.pt_like, p.pt_hit ");
+		sb.append("from post p, image i ");
+		sb.append("where p.pt_no=i.pt_no ");
+		sb.append("order by pt_hit desc) ");
+		sb.append("where rownum<=3 ");
+		
+		ImageVo vo=null;
+		try {
+			pstmt=conn.prepareStatement(sb.toString());
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+			
+			String pt_place=rs.getString("pt_place");
+			int pt_hit=rs.getInt("pt_hit");
+			int pt_like=rs.getInt("pt_like");
+			String img_temp1=rs.getString("img_temp1");
+			String pt_title=rs.getString("pt_title");
+			
+			vo=new ImageVo(pt_place, img_temp1, pt_title, pt_like, pt_hit);
+			list.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+}//class end
---
+++ mainPage.jsp
@@ -0,0 +1,179 @@
+<%@page import="Vo.Mem"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="Vo.ImageVo"%>
+<%@page import="Dao.ImageDao"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%
+    	ImageDao dao=new ImageDao();
+    	ArrayList<ImageVo> list=dao.getLikeThree();
+    	ArrayList<ImageVo> list1=dao.getHitThree();
+    %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<style type="text/css">
+	body{
+		margin-left: 250px;
+	}
+	#wrapper{
+		background-color: #f9f9f9;
+		width: 1000px;
+	}
+	#head{
+		background-color: pink;
+		height: 200px;
+	}
+	#nav{
+		width: 1000px;
+		height: 320px;
+		padding: 0 0 0 35px;
+	}
+	#nav1{
+		width: 1000px;
+		height: 350px;
+		padding: 0 0 0 35px;
+	}
+	#search{
+		margin-left: 300px;
+		margin-top: 90px;
+		width: 400px;
+		height: 50px;
+		border-radius: 25px;
+		border : 4px solid #A4EBFF;
+		font-size: 20px;
+	}
+	 #button{
+	 	border-radius: 25px;
+	 	border: 2px solid #A4EBFF;
+	 	background-color: white;
+	 	height: 60px;
+	 	position: absolute;
+	 	left: 895px;
+	 	top: 100px;
+	 	width: 65px;
+	 	background-color: #A4EBFF;
+	 	color: white;
+	 	font-size: 15px;
+	}
+	.strapline {
+		color: black;
+		font-size: 25px;
+		font-family: '굴림';
+	}
+	#footer{
+		border-top: 1px solid black;
+		margin-bottom: 100px;
+	}
+	#footimg{
+		margin-top: 10px;
+		margin-left: 10px;
+		width: 40px;
+		height: 20px;
+	}
+	#notice{
+		text-decoration: none;
+	}
+	#inner_foot{
+		margin-left: 20px;
+	}
+	#head1{
+		width: 80px;
+		height: 30px;
+		position: absolute;
+		top: 9px;		
+	}
+	#membership{
+		position: absolute;
+		text-decoration: none;
+		left: 1100px;
+		top: 10px;
+	}
+	#login{
+		position: absolute;
+		text-decoration: none;
+		left: 1190px;
+		top: 10px;
+	}
+	#MyPage{
+		position: absolute;
+		text-decoration: none;
+		left: 1020px;
+		top: 10px;		
+	}
+	
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript">
+	$(function(){		
+		$("img").each(function(i){
+			var width=parseInt($(".imgs").css("width"));
+			var height=parseInt($(".imgs").css("height"));
+			
+				if(width>100&height>100){
+					$(".imgs").css("width",300);
+					$(".imgs").css("height",250);
+				}//if end		
+		});//each end
+		
+		$("#top").hide();
+		var scrollStandard=150;
+		var top=parseInt($("#top").css("top"));
+		
+	});//function end
+</script>
+</head>
+<body>
+<form action="search.jsp" method="get">
+	<div id="wrapper">
+		<div id="head">
+			<input type="text" name="search" id="search" autofocus="" />
+			<input type="submit" value="검색" id="button"/>
+		</div>
+		<div>
+			<img src="../images/mango.jpg" alt="로고" id="head1"/>
+		</div>
+		
+		<div>
+			<a href="" id="membership">회원가입</a>
+		</div>
+		<div>
+			<a href="" id="login">로그인</a>
+		</div>
+		<div>
+			<a href="" id="MyPage">MyPage</a>
+		</div>
+		
+		<div id="nav">
+				<h1 class="strapline">Best데이트</h1>
+		<%
+		for(ImageVo vo : list){
+		%>
+				<a href="">
+				<img src="<%=vo.getImg_temp1()%>" alt="<%=vo.getPt_title() %>" class="imgs" /></a>
+				&nbsp;&nbsp;
+		<%}%>
+		</div>
+		<div id="nav1">
+				<h1 class="strapline">핫플레이스</h1>
+		<%
+		for(ImageVo vo1 : list1){
+		%>
+				<a href="#">
+				<img src="<%=vo1.getImg_temp1() %>" alt="<%=vo1.getPt_title() %>" class="imgs" /></a>
+				&nbsp;&nbsp;
+		<%}%>
+		</div>	
+	
+		<div id="footer">
+			<div id="inner_foot">
+				<img src="../images/mango.jpg" alt="로고" id="footimg" />&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="" id="notice"><font weight='bold'>공지사항</font>> Daco운영정책 변경 안내</a>
+				<p><font size="2" color="grey">회사소개 ·광고안내·검색등록·제휴문의·인재채용·서비스약관·청소년보호정책·개인정보처리방침·웹접근성안내·고객센터</font></p>
+				<p><font size="2" color="grey">Copyright © Daco Corp. All rights reserved.</font></p>
+			</div>		
+</form>
+</body>
+</html>
